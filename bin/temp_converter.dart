fbf947f42a862d7e248c6cfc41828eb508bbc490
A R Luzikooy <reinald.luzikooij@gmail.com>
2021-10-22 11:18:53 +0700

    init

---
+++ bin/temp_converter.dart
@@ -0,0 +1,5 @@
+import 'package:temp_converter/temp_converter.dart' as temp_converter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${temp_converter.calculate()}!');
+}
---
+++ test/temp_converter_test.dart
@@ -0,0 +1,8 @@
+import 'package:temp_converter/temp_converter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
