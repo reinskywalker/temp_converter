a48f8a139e536f35d12e79969acb88531579001c
A R Luzikooy <reinald.luzikooij@gmail.com>
2021-10-22 12:40:27 +0700

    add celcius reamur
    C × 4/5

--- bin/temp_converter.dart
+++ bin/temp_converter.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 main (List<String> arguments) {
-  print("Temperature Converter from C to F and K, select : \n1 : Celcius to Fahrenheit\n2 : Celcius to Kelvin");
+  print("Temperature Converter from C to F and K, select : \n1 : Celcius to Fahrenheit\n2 : Celcius to Kelvin\n3 : Celcius to Kelvin");
   String input = stdin.readLineSync();
   int type = int.tryParse(input);
   double result;
@@ -12,7 +12,7 @@ main (List<String> arguments) {
       String input = stdin.readLineSync();
       double fahrenheit = double.tryParse(input);
       var result = ((9/5)* fahrenheit + 32);
-      print("Result from C -> F is $result F");
+      print("Result from C to F is $result F");
       return result;
 
     case 2:
@@ -20,10 +20,18 @@ main (List<String> arguments) {
       String input = stdin.readLineSync();
       double kelvin = double.tryParse(input);
       var result = (kelvin + 273.15);
-      print("Result from C -> K is $result K");
+      print("Result from C to K is $result K");
       return result;
 
+    case 3:
+      print("C to R chosen, please type your C temp = ");
+      String input = stdin.readLineSync();
+      double reamur = double.tryParse(input);
+      var result = ((reamur*4)/5);
+      print("Result from C to R is $result R");
+      return result;
+      
     default:
-      print("Please select either 1 or 2");
+      print("Please select either 1, 2, or 3");
   }
 }
