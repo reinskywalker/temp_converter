aaddbffd8cff45a62c07ed6764cde48987d09817
A R Luzikooy <reinald.luzikooij@gmail.com>
2021-10-22 11:29:03 +0700

    add formula
    F = (9/5) C + 32

--- bin/temp_converter.dart
+++ bin/temp_converter.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
 main (List<String> arguments) {
-  print("Enter Celciius Temp : ");
+  print("Enter Celcius Temp : ");
   String input = stdin.readLineSync();
+  double fahrenheit = double.tryParse(input);
+  var result = ((9/5) * fahrenheit + 32);
 
+  print("Your temperature in Fahrenheit is $result F");
 }
